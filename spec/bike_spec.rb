41b395a06774d2f20465916e6dde853a3eeac6bf
thejennywang <thejennywang@gmail.com>
2014-07-07 16:34:06 +0100

    Jenny & Toan | Added bike class

---
+++ spec/bike_spec.rb
@@ -0,0 +1,22 @@
+require 'bike'
+
+describe Bike do
+	
+	let (:bike) { Bike.new }
+
+	it 'should not be broken when created' do
+		expect(bike).not_to be_broken
+	end
+
+	it 'should be able to break' do
+		bike.break!
+		expect(bike).to be_broken
+	end
+
+	it 'should be able to fix' do
+		bike.break!
+		bike.fix!
+		expect(bike).not_to be_broken
+	end
+
+end
