a1e88f637af73e0551d8086203c14539140fe8ac
thejennywang <thejennywang@gmail.com>
2014-07-07 19:10:08 +0100

    updated the bike container spec

--- spec/bike_container_spec.rb
+++ spec/bike_container_spec.rb
@@ -7,6 +7,8 @@ describe BikeContainer do
 	
 	let(:bike) { Bike.new }
 	let(:holder) { ContainerHolder.new }
+	let(:working_bike) { Bike.new }
+	let(:broken_bike) { Bike.new.break! }
 
 	it 'should accept a bike' do
 		expect(holder.bike_count).to eq 0
@@ -19,5 +21,16 @@ describe BikeContainer do
 		expect(holder.bike_count).to eq 1
 		holder.release(bike)
 		expect(holder.bike_count).to eq 0
+	end
+
+	it 'should not accept bikes if full' do
+		10.times { holder.dock(Bike.new) }
+		expect( lambda { holder.dock(bike) } ).to raise_error(RuntimeError)
+	end
+
+	it 'should know which bikes are available' do
+		holder.dock(working_bike)
+		holder.dock(broken_bike)
+		expect(holder.available_bikes).to eq([working_bike])
 	end
 end
